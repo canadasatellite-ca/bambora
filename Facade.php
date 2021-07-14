d3f5323b76842c78d6a99a022296d975e4c4de9d
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-14 11:22:08 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- Facade.php
+++ Facade.php
@@ -108,7 +108,6 @@ final class Facade {
 		$res = new _DO;
 		$reqA = $req->getData();
 		$resA2 = [
-			0 => '1',
 			1 => '1',
 			2 => '1',
 			3 => '(TESTMODE) This transaction has been approved.',
@@ -211,7 +210,6 @@ final class Facade {
 		$resA2[29] = $reqA['x_ship_to_state'];
 		$resA2[30] = $reqA['x_ship_to_zip'];
 		$resA2[31] = $reqA['x_ship_to_country'];
-		$resA2[0] = '1';
 		$resA2[1] = '1';
 		$resA2[2] = '1';
 		$resA2[3] = '(TESTMODE2) This transaction has been approved.';
@@ -221,7 +219,6 @@ final class Facade {
 		$resA2[37] = '382065EC3B4C2F5CDC424A730393D2DF';
 		$resA2[39] = '';
 		$resA = $this->beanstreamapi($reqA, $type); /** @var array(string => mixed) $resA */
-		$resA2[0] = $resA['response_code'];
 		$resA2[1] = $resA['response_subcode'];
 		$resA2[2] = $resA['response_reason_code'];
 		$resA2[3] = $resA['response_reason_text'];
@@ -233,7 +230,7 @@ final class Facade {
 		if (!$resA2) {
 			df_error('Error in payment gateway');
 		}
-		$res->setResponseCode((int)str_replace('"', '', $resA2[0]));
+		$res->setResponseCode((int)str_replace('"', '', $resA['response_code']));
 		$res->setResponseSubcode((int)str_replace('"', '', $resA2[1]));
 		$res->setResponseReasonCode((int)str_replace('"', '', $resA2[2]));
 		$res->setResponseReasonText($resA2[3]);
