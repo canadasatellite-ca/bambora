c087fc2256b74a6521be0dc3870e6d795355a4fa
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-14 11:42:45 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- Facade.php
+++ Facade.php
@@ -108,7 +108,6 @@ final class Facade {
 		$res = new _DO;
 		$reqA = $req->getData();
 		$resA2 = [
-			4 => '000000',
 			5 => 'P',
 			6 => '0',
 			7 => '100000018',
@@ -207,13 +206,11 @@ final class Facade {
 		$resA2[29] = $reqA['x_ship_to_state'];
 		$resA2[30] = $reqA['x_ship_to_zip'];
 		$resA2[31] = $reqA['x_ship_to_country'];
-		$resA2[4] = '000000';
 		$resA2[5] = 'P';
 		$resA2[6] = '0';
 		$resA2[37] = '382065EC3B4C2F5CDC424A730393D2DF';
 		$resA2[39] = '';
 		$resA = $this->beanstreamapi($reqA, $type); /** @var array(string => mixed) $resA */
-		$resA2[4] = $resA['approval_code'];
 		$resA2[5] = $resA['avs_result_code'];
 		$resA2[6] = $resA['transaction_id'];
 		$resA2[37] = $resA['md5_hash'];
@@ -222,7 +219,7 @@ final class Facade {
 		$res->setResponseSubcode((int)str_replace('"', '', $resA['response_subcode']));
 		$res->setResponseReasonCode((int)str_replace('"', '', $resA['response_reason_code']));
 		$res->setResponseReasonText($resA['response_reason_text']);
-		$res->setApprovalCode($resA2[4]);
+		$res->setApprovalCode($resA['approval_code']);
 		$res->setAvsResultCode($resA2[5]);
 		$res->setTransactionId($resA2[6]);
 		$res->setInvoiceNumber($resA2[7]);
