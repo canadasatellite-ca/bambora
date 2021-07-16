777a056cd2193638b744172a9eaf139556b5732b
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-17 04:49:25 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

---
+++ Response.php
@@ -0,0 +1,5 @@
+<?php
+namespace CanadaSatellite\Bambora;
+# 2021-07-17 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+# "Refactor the `Schogini_Beanstream` module": https://github.com/canadasatellite-ca/bambora/issues/1
+final class Response extends \Df\Core\O {}
