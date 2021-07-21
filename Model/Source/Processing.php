92d51eec160c6c1cf7df525ebaa43d0821758c51
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-21 17:47:20 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- Model/Source/Processing.php
+++ Model/Source/Processing.php
@@ -1,2 +1,8 @@
 <?php
-namespace CanadaSatellite\Bambora\Model\Source; class Processing extends \Magento\Sales\Model\Config\Source\Order\Status { protected $_stateStatuses = array(\Magento\Sales\Model\Order::STATE_PENDING_PAYMENT, \Magento\Sales\Model\Order::STATE_PROCESSING, \Magento\Sales\Model\Order::STATE_COMPLETE, \Magento\Sales\Model\Order::STATE_CLOSED); }
+namespace CanadaSatellite\Bambora\Model\Source;
+use Magento\Sales\Model\Order as O;
+# 2021-07-21 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+# "Refactor the `Schogini_Beanstream` module": https://github.com/canadasatellite-ca/bambora/issues/1
+class Processing extends \Magento\Sales\Model\Config\Source\Order\Status {
+	protected $_stateStatuses = [O::STATE_PENDING_PAYMENT, O::STATE_PROCESSING, O::STATE_COMPLETE, O::STATE_CLOSED];
+}
