29de95a55d673d33007b0df6a6bdfb9288db9b3f
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-21 17:54:46 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- Model/BeanstreamException.php
+++
@@ -1,2 +0,0 @@
-<?php
-namespace CanadaSatellite\Bambora\Model; class BeanstreamException extends \Magento\Framework\Exception\LocalizedException { const AUTHENTICATION_ERROR = 'An authentication error occurred.'; }
