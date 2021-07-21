ae7db9d84b1bf9d48bad265b54c1a80e47729818
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-21 18:27:21 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- view/frontend/web/js/view/payment/method-renderer/beanstream.js
+++ view/frontend/web/js/view/payment/method-renderer/beanstream.js
@@ -1,24 +1,5 @@
-/*browser:true*/
-/*global define*/
-define(
-    [
-        'Magento_Payment/js/view/payment/cc-form' // instead of payment/default we are inheriting from payment/cc-form since, we want to use the default Magento CC Form
-    ],
-    function (Component) {
-        'use strict';
-        // console.log('I am in the beanstream.js file');
-        return Component.extend({
-            defaults: {
-                //template: 'Magento_Payment/payment/cc-form' // Nothing fancy here. We want to use the default Magento CC Form
-                template: 'CanadaSatellite_Bambora/payment/cc-form'
-            },
-             getCode: function() {
-                return 'beanstream'; // Our payment module code. Should be the same as what we give in our Model
-            },
-            isActive: function() {
-                return true;
-            }
-        });
-    }
-);
-
+define(['Magento_Payment/js/view/payment/cc-form'], function (Component) {'use strict'; return Component.extend({
+	defaults: {template: 'CanadaSatellite_Bambora/payment/cc-form'}
+	,getCode: function() {return 'beanstream';}
+	,isActive: function() {return true;}
+});});
