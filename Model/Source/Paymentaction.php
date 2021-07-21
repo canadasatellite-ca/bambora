938893a361fb7f28d4ee605622031b0fa9f43bd2
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-21 17:43:21 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- Model/Source/Paymentaction.php
+++ Model/Source/Paymentaction.php
@@ -1,9 +1,15 @@
 <?php
 namespace CanadaSatellite\Bambora\Model\Source;
-class Paymentaction
-{
-    function toOptionArray()
-    {
-        return array(array('value' => \Magento\Payment\Model\Method\Cc::ACTION_AUTHORIZE, 'label' => 'Authorize Only'), array('value' => \Magento\Payment\Model\Method\Cc::ACTION_AUTHORIZE_CAPTURE, 'label' => 'Sale'));
-    }
+use Magento\Payment\Model\Method\Cc;
+# 2021-07-21 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+# "Refactor the `Schogini_Beanstream` module": https://github.com/canadasatellite-ca/bambora/issues/1
+class Paymentaction {
+	/**
+	 * 2021-07-21
+	 * @return array(array(string => string))
+	 */
+	function toOptionArray() {return [
+		['value' => Cc::ACTION_AUTHORIZE, 'label' => 'Authorize Only']
+		,['value' => Cc::ACTION_AUTHORIZE_CAPTURE, 'label' => 'Sale']
+	];}
 }
